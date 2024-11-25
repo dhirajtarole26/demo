e3a3b0c92513112df9b40b9dbe2ab1f994d3b3fc
dhirajtarol26 <dhirajtarol9@gmail.com>
2024-11-25 19:33:20 +0530

    Array file update_1

---
+++ array.c
@@ -0,0 +1,22 @@
+//printing thr array and its reverse
+#include<stdio.h>
+void main()
+{
+    int a[5]={10,20,30,40,50},element,i;
+    printf("%ld\n",sizeof(a));
+    printf("%ld\n",sizeof(a[0]));
+    element=sizeof(a)/sizeof(a[0]);
+
+    for(i=0;i<element;i++)
+    {
+        //printf("%d\n",a[i]);
+        printf("a[%d]=%d\n",i,a[i]);
+    }
+    printf("\n");
+    for(i=element-1;i>=0;i--)
+    {
+        //printf("%d\n",a[i]);
+         printf("a[%d]=%d\n",i,a[i]);
+    }
+    
+}
